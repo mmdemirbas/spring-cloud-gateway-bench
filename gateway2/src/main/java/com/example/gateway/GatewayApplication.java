5c63b4477eb32490b1f213769f7064a21ae425ed
Muhammed Demirbaş <muhammed@opsgenie.com>
2018-03-14 15:04:28 +0300

    Rename gateway to gateway2 - preparing for gateway1

--- gateway2/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway2/src/main/java/com/example/gateway/GatewayApplication.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.client.HttpClient;
 import reactor.ipc.netty.http.client.HttpClientOptions;
 import reactor.ipc.netty.resources.PoolResources;
@@ -30,16 +27,6 @@ public class GatewayApplication {
 			// opts.disablePool(); //TODO: why do I need this again?
             // opts.poolResources(PoolResources.fixed("proxy"));
 		};
-	}
-
-	@Bean
-	public RouteLocator myRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(r -> r.path("/**")
-						// .filter((exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() ->
-						// 		exchange.getResponse().getHeaders().add("Connection", "close"))))
-						.uri("http://localhost:8000"))
-				.build();
 	}
 
 	public static void main(String[] args) {
